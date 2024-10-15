19cfc033eb1b32440b67726e46ba8ffc984902dd
Madhankumar-7923 <madhankumar13148@gmail.com>
2024-10-15 19:51:17 +0530

    vercel setup

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ import axios from "axios";
 import weatherData from "./utils/weatherData.js";
 import env from "dotenv";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 env.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
+
+// Get the directory name from the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
-app.set("view engine", "ejs")
+// set the public files directory
+app.use(express.static(path.join(__dirname, 'public')));
+/*app.use(express.static("public"));*/
+
+app.set("view engine", "ejs");
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
     res.render("index");
